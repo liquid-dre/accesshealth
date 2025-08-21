37891e605c21615d116c5ed5dc8a5a88957d6588
liquid-dre <avdingiswayo54@gmail.com>
2025-08-21 21:52:06 +0200

    style: ECG Path

--- app/resource/[[...resourceID]]/_components/resource-page-client.tsx
+++ app/resource/[[...resourceID]]/_components/resource-page-client.tsx
@@ -97,13 +97,14 @@ export const ResourceClientPage = ({
 				fill="none"
 				viewBox="0 0 100 1000"
 			>
+				{/* ECG Line */}
 				<path
 					ref={pathRef}
-					d="M50 0 Q75 100 50 200 T50 400 T50 600 T50 800 T50 1000"
+					d="M50 0 L50 80 L45 90 L50 100 L50 120 L90 130 L50 140 L20 150 L50 160 L50 200 L45 210 L50 220 L50 240 L90 250 L50 260 L20 270 L50 280 L50 320 L45 330 L50 340 L50 360 L90 370 L50 380 L20 390 L50 400 L50 440 L45 450 L50 460 L50 480 L90 490 L50 500 L20 510 L50 520 L50 560 L45 570 L50 580 L50 600 L90 610 L50 620 L20 630 L50 640 L50 680 L45 690 L50 700 L50 720 L90 730 L50 740 L20 750 L50 760 L50 800 L45 810 L50 820 L50 840 L90 850 L50 860 L20 870 L50 880 L50 920 L45 930 L50 940 L50 960 L90 970 L50 980 L20 990 L50 1000"
 					className="squiggle stroke-gray-300"
 					style={{
 						strokeWidth: 2,
-						strokeDasharray: "4 8",
+						strokeDasharray: "none",
 						willChange: "stroke-dashoffset",
 					}}
 				/>
